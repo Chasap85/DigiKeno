1aeceef4bf6ceb471173b984c68d05d09f21ad6c
Chase Little <chaselittle@Chases-MacBook-Pro-2.local>
2024-05-31 19:44:13 -0700

    config updated

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,18 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {languageOptions: { globals: globals.browser}},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
   {
     files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
-    ...pluginReactConfig,
     rules: {
+      ...pluginReactConfig.rules,
       "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off",
     },
   },
 ];
